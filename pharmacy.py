70a5818f233d491fda389f7a860ec2654058d900
yaroleana7-maker <yaroleana7@gmail.com>
2026-05-26 22:14:02 +0000

    Ajout du code

--- pharmacy.py
+++ pharmacy.py
@@ -1,1 +1,9 @@
-# Code will  be  stage here .
+def add_product (stock: list) -> None:
+    display_separator("ADD A PRODUCT")
+    print(" 1. Medication")
+    print(" 2. Parapharmacy Product")
+    choice = input(" Your choice: ").strip()
+    
+    name = input(" Product name: ").strip()
+    price = input_float(" Price(FCFA): ")
+    quantity = input_integer( "Quantity: ")
