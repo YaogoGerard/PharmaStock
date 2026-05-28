58fab255b3093c2753cb7fb005af5a30b0d70cf6
yaroleana7-maker <yaroleana7@gmail.com>
2026-05-28 11:37:52 +0000

    Ajout du code pharmacie

--- pharmacy.py
+++ pharmacy.py
@@ -1,9 +1,9 @@
 def add_product (stock: list) -> None:
-    display_separator("ADD A PRODUCT")
+    display_separator ("ADD A PRODUCT")
     print(" 1. Medication")
     print(" 2. Parapharmacy Product")
     choice = input(" Your choice: ").strip()
     
     name = input(" Product name: ").strip()
     price = input_float(" Price(FCFA): ")
-    quantity = input_integer( "Quantity: ")
+    quantity = input_integer ( "Quantity: ")
